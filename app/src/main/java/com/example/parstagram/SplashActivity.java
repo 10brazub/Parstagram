48d27e7f605c40be54f5d92a0a0e2f0b5a1b6c4a
Brandon Zubia <brandonz@fb.com>
2022-06-13 15:18:22 -0700

    Initial commit

---
+++ app/src/main/java/com/example/parstagram/SplashActivity.java
@@ -0,0 +1,34 @@
+package com.example.parstagram;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import java.util.Objects;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        Thread splashScreenStarter = new Thread(() -> {
+            try {
+                int delay = 0;
+                while (delay < 1500) {
+                    Thread.sleep(150);
+                    delay = delay + 100;
+                }
+                startActivity(new Intent(SplashActivity.this, LoginActivity.class));
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } finally {
+                finish();
+            }
+        });
+        splashScreenStarter.start();
+        Objects.requireNonNull(getSupportActionBar()).hide();
+    }
+}
